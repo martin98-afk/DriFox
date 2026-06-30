9db1656a5c5a6ed1b3a586d0624757c4088c34bf
mading <martin98@bu.edu>
2026-06-30 16:36:28 +0800

    chore: update version to v0.2.14 in config and installer files

--- app/utils/config.py
+++ app/utils/config.py
@@ -249,7 +249,7 @@ class Settings(QConfig):
     auto_start = ConfigItem("General", "AutoStart", False, BoolValidator())
 
     # 版本信息
-    current_version = "v0.2.13"
+    current_version = "v0.2.14"
     # 通用设置
     auto_check_update = ConfigItem("General", "AutoCheckUpdate", True, BoolValidator())
 
