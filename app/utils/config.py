a77a5e9b9e9cfbe5fdbc238036d299aafcfd8835
dingma <blackma2009@163.com>
2026-07-24 00:04:06 +0800

    chore: update version to v0.4.6 in config, installer and readme files

--- app/utils/config.py
+++ app/utils/config.py
@@ -311,7 +311,7 @@ class Settings(QConfig):
     auto_start = ConfigItem("General", "AutoStart", False, BoolValidator())
 
     # 版本信息
-    current_version = "v0.4.5"
+    current_version = "v0.4.6"
     # 通用设置
     auto_check_update = ConfigItem("General", "AutoCheckUpdate", True, BoolValidator())
 
