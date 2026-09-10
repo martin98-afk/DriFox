adf3a3e4d15c0777f56302dc9879755eb2cb82f4
mading <martin98@bu.edu>
2026-09-10 15:53:14 +0800

    revert: roll back v0.5.11 verification release to v0.5.10

--- app/utils/config.py
+++ app/utils/config.py
@@ -306,7 +306,7 @@ class Settings(QConfig):
     auto_start = ConfigItem("General", "AutoStart", False, BoolValidator())
 
     # 版本信息
-    current_version = "v0.5.11"
+    current_version = "v0.5.10"
     # 通用设置
     auto_check_update = ConfigItem("General", "AutoCheckUpdate", True, BoolValidator())
 
